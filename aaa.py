b3021a784891a77275ee9896cdbcdcd1c2a69768
ayrus241 <53077799+ayrus241@users.noreply.github.com>
2019-07-19 09:05:53 +0000

    Update and rename aaa.py to bbb.py

--- aaa.py
+++
@@ -1,8 +0,0 @@
-number=int(input())
-if (number>0):
-    if (number%2==0):
-        print("Even")
-    elif (number%2!=0):
-        print("Odd")
-else:
-    print("invalid")
---
+++ bbb.py
@@ -0,0 +1,9 @@
+num=input()
+if num in ['a','e','i','o','u']:
+    print("Vowel")
+elif num in ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']:
+    print("Consonant")
+else:
+    print("invalid")
+
+    
