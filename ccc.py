2e80213f88560fdac1f284296acaea63cdb6eb82
ayrus241 <53077799+ayrus241@users.noreply.github.com>
2019-07-19 09:16:12 +0000

    Update and rename ccc.py to dd.py

--- ccc.py
+++
@@ -1,7 +0,0 @@
-num=input()
-if num in ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']:
-    print("Alphabet")
-else:
-    print("No")
-
-    
